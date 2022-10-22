83e60462fb01d1faf9c6cfe46311565381de864c
Christian Del Rosario <ainsley.drosario@gmail.com>
2022-10-22 12:00:35 +0800

    Added FaceRecognition.py

---
+++ FaceRecognition.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+from keras.models import load_model
+
+model = load_model('FaceTracker.h5')
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _ , frame = cap.read()
+    frame = frame[50:500, 50:500,:]
+    
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    resized = tf.image.resize(rgb, (120,120))
+    
+    yhat = model.predict(np.expand_dims(resized/255,0))
+    sample_coords = yhat[1][0]
+    
+    if yhat[0] > 0.5: 
+        # Controls the main rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.multiply(sample_coords[:2], [450,450]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [450,450]).astype(int)), 
+                            (255,0,0), 2)
+        # Controls the label rectangle
+        cv2.rectangle(frame, 
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int), 
+                                    [0,-30])),
+                      tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                    [80,0])), 
+                            (255,0,0), -1)
+        
+        # Controls the text rendered
+        cv2.putText(frame, 'Ains', tuple(np.add(np.multiply(sample_coords[:2], [450,450]).astype(int),
+                                               [0,-5])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+    
+    cv2.imshow('Face Recognition', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
